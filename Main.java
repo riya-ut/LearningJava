94154655ca6d0e76d39a89635b21a1a4e20f575c
Riya UT <156648774+riya-ut@users.noreply.github.com>
2024-04-03 17:37:32 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package myFirstProject;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	{int m= scanner.nextInt();
+	int n= scanner.nextInt();
+	int sum= m+n;
+	System.out.println(sum);
+	scanner.close();
+	}
+	}
+	
+	}
