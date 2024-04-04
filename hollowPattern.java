9a8a2f71e40fd0c9cec0cd486e641c1065b4bbeb
Riya UT <156648774+riya-ut@users.noreply.github.com>
2024-04-04 22:00:18 +0530

    Add files via upload

---
+++ hollowPattern.java
@@ -0,0 +1,19 @@
+package myFirstProject;
+
+public class hollowPattern {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+
+        }
+
+    }
+}
