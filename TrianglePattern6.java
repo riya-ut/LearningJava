ac84aa21bc90e642c460a87183d723226f1ae0d1
Riya UT <156648774+riya-ut@users.noreply.github.com>
2024-04-14 10:19:33 +0530

    Add files via upload

--- TrianglePattern6.java
+++ TrianglePattern6.java
@@ -1,15 +1,15 @@
 package myFirstProject;
 
-public class TrianglePattern4 {
+public class TrianglePattern6 {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 9;
         for (int i = 1; i <= n; i++) {
             for (int k = 1; k <= n - i; k++) {
                 System.out.print(" ");
             }
             {
                 for (int j = 1; j <= i; j++) {
-                    System.out.print("*");
+                    System.out.print("* ");
                 }
                 System.out.println();
 
@@ -18,4 +18,5 @@ public class TrianglePattern4 {
         }
 
     }
+
 }
