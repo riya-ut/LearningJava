158a9a430f0f61e21abf8fb2b2b04d8eeac219ce
Riya UT <156648774+riya-ut@users.noreply.github.com>
2025-01-01 21:50:57 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,10 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        byte a = 45;
+        short b;
+        b = a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
