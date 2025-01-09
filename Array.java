e415133c869e84ad9ea2e753ee41c2d3fe4f3e9e
Riya UT <156648774+riya-ut@users.noreply.github.com>
2025-01-09 23:49:20 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,9 @@
+class Array {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        System.out.println(a[0]);
+        a[3] = 20;
+        System.out.println(a[3]);
+
+    }
+}
