70432c781b5baab9060b5ebdd32444c6d058c76b
Riya UT <156648774+riya-ut@users.noreply.github.com>
2025-01-25 23:49:01 +0530

    Add files via upload

---
+++ Concatenation.java
@@ -0,0 +1,16 @@
+public class Concatenation {
+
+    public static void main(String[] args) {
+        System.out.println("Java".concat("JavaScript"));
+        String s1 = "HTML";
+        String s2 = "CSS";
+
+        System.out.println(s1.concat(s2));
+
+        String s3 = "5";
+        String s4 = "10";
+
+        System.out.println(s3.concat(s4));
+    }
+
+}
